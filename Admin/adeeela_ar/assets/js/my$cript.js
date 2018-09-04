36667414095552a5b90db264119d8b9f116100a1
MufasaAdam <mustafajeja@gmail.com>
2018-09-04 12:33:49 +0200

    Legacy Code

--- Admin/adeeela_ar/assets/js/my$cript.js
+++ Admin/adeeela_ar/assets/js/my$cript.js
@@ -65,3 +65,11 @@ function searchFilter() {
 }
 
 //
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true
+});
+
+$(document).ready(function () {
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+});
